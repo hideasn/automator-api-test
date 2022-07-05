027b2624038aac6e4806eec63ab63a21ae6f6420
Hide <2037456404@qq.com>
2022-07-05 08:36:38 +0800

    新增Demo测试文件

---
+++ src/main/java/com/hide/javainterfaceproject/test/Demo.java
@@ -0,0 +1,14 @@
+package com.hide.javainterfaceproject.test;
+
+/**
+ * @program: java-interface-project
+ * @ClassName: Demo
+ * @description: 测试用
+ * @author: HideAsn
+ * @create: 2022-07-05
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
